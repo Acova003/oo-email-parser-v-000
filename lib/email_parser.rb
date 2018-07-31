e25e3df4b780dd22cb9f02c4608b1f52472255fd
Amee Covarrubias <ameecova@yahoo.com>
2018-07-31 09:00:20 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,10 +6,6 @@ require 'pry'
 class EmailParser
   attr_accessor :emails
   
-  def initialize(emails)
-    @emails = emails
-  end 
-  
   def self.parse
     binding.pry
     @emails.split(/[,\s]+/)
